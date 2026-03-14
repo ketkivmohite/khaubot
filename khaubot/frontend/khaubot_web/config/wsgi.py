848e60814a0dffe918817bce8d8662ac2525012f
ketkivmohite <ketkimohite214@gmail.com>
2026-03-15 00:52:32 +0530

    run migrations on vercel startup

--- khaubot/frontend/khaubot_web/config/wsgi.py
+++ khaubot/frontend/khaubot_web/config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+
+# Run migrations automatically on Vercel startup
+if os.getenv("VERCEL"):
+    from django.core.management import call_command
+    try:
+        call_command("migrate", interactive=False)
+    except Exception as e:
+        print("Migration error:", e)
